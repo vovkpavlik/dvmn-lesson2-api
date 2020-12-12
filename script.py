5ac9596370103e4dd98d40a4c7ef3feaebbd215b
vovkpavlik <vovk.pavlik@bk.ru>
2020-12-12 18:23:42 +0300

    Перенес функцию env в блок if__main__.

--- script.py
+++ script.py
@@ -1,13 +1,12 @@
+import env as env
 import requests
 from urllib.parse import urlparse
 
 from environs import Env
 
-env = Env()
-env.read_env()
 
-TOKEN = env.str("TOKEN")
-headers = {"Authorization": f"Bearer {TOKEN}"}
+TOKEN = env.str("BITLY_TOKEN")
+HEADERS = {"Authorization": f"Bearer {TOKEN}"}
 
 
 def shorten_link(users_link):
@@ -16,7 +15,7 @@ def shorten_link(users_link):
         "long_url": users_link,
         "domain": "bit.ly",
     }
-    short_site = requests.post(url, headers=headers, json=body)
+    short_site = requests.post(url, headers=HEADERS, json=body)
     short_site.raise_for_status()
     return short_site.json()["link"]
 
@@ -24,20 +23,23 @@ def shorten_link(users_link):
 def count_clicks(short_link):
     parsed_link = urlparse(short_link)
     url = f"https://api-ssl.bitly.com/v4/bitlinks/{parsed_link.netloc}{parsed_link.path}/clicks/summary"
-    total_clicks = requests.get(url, headers=headers)
+    total_clicks = requests.get(url, headers=HEADERS)
     total_clicks.raise_for_status()
     return total_clicks.json()["total_clicks"]
 
-def get_bitlink_info(short_link):
+def check_if_binlink(short_link):
     parsed_link = urlparse(short_link)
     url = f"https://api-ssl.bitly.com/v4/bitlinks/{parsed_link.netloc}{parsed_link.path}"
-    info_link = requests.get(url, headers=headers)
+    info_link = requests.get(url, headers=HEADERS)
     return info_link.ok
 
 
 if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+
     users_link = input("Введите адрес сайта: ")
-    if get_bitlink_info(users_link):
+    if check_if_binlink(users_link):
         try:
             clicks_count = count_clicks(users_link)
             print(f"Количество кликов: {clicks_count}")
